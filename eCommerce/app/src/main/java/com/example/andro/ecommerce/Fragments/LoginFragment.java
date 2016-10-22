c66e1d8b67e8c2141654ee56e766bdb236c763ad
Bruce <wyd248@gmail.com>
2016-10-21 23:29:48 -0500

    branch test

--- eCommerce/app/src/main/java/com/example/andro/ecommerce/Fragments/LoginFragment.java
+++ eCommerce/app/src/main/java/com/example/andro/ecommerce/Fragments/LoginFragment.java
@@ -79,7 +79,7 @@ public class LoginFragment extends Fragment {
 
         Log.i("masterbranch", "this is from master branch!");
 
-        Log.i("localbranch", "this is from dada branch");
+        Log.i("localbranch", "this is from dada branch and change something");
 
         // Tag used to cancel the request
         String tag_json_obj = "json_obj_req";
