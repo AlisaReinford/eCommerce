b137ae375890c242505d2008381ce76c30f007e1
Bruce <wyd248@gmail.com>
2016-10-21 23:43:19 -0500

    commit before revert

--- eCommerce/app/src/main/java/com/example/andro/ecommerce/Fragments/LoginFragment.java
+++ eCommerce/app/src/main/java/com/example/andro/ecommerce/Fragments/LoginFragment.java
@@ -79,6 +79,10 @@ public class LoginFragment extends Fragment {
 
         Log.i("masterbranch", "something changed from master branch!");
         
+        Log.i("masterbranch", "this is from master branch!");
+
+        Log.i("localbranch", "this is from dada branch");
+
         // Tag used to cancel the request
         String tag_json_obj = "json_obj_req";
         pDialog = new ProgressDialog(getContext(), R.style.AppTheme_Dark_Dialog);
